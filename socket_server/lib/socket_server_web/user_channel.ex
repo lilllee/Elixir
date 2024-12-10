19e7265fccd6dd7f51a632c42ae73d16795844e9
junholee <kopqwe147@knowwheresoft.com>
2024-12-10 13:40:09 +0900

    fix ets table name

--- socket_server/lib/socket_server_web/user_channel.ex
+++ socket_server/lib/socket_server_web/user_channel.ex
@@ -7,9 +7,8 @@ defmodule SocketServerWeb.UserChannel do
   end
 
   def handle_in("send_message", %{"body" => body}, socket) do
-
     if Presence.list("admin:lobby") |> map_size() === 0 do
-      :ets.insert(:cache_table, {:"msg_#{:os.system_time(:millisecond)}", body})
+      :ets.insert(:admin, {:"msg_#{:os.system_time(:millisecond)}", body})
     else
       broadcast!(socket, "admin:lobby", %{"user_id" => socket.assigns.user_id, "body" => body})
     end
